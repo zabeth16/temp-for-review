0d1d73df7eb96bacb1adbe8f39a485ab0d22e00b
ZABETH <40664034+zabeth16@users.noreply.github.com>
2022-11-25 10:58:02 +0800

    無限輪迴page = 0

--- static/index.js
+++ static/index.js
@@ -1,6 +1,8 @@
 // 老師說要放，但不確定放哪裡，先擺著
 let isLoading = false;
 
+let page_num = 0
+
 function get_data(){
     
 
@@ -11,9 +13,9 @@ function get_data(){
     // for (page_num = 0 ; page_num < 4 ; page_num ++ ){
     //     console.log(page_num)
     // }
-    let page_num = 0
+    
 
-    fetch("/api/attractions?page=" + `${page_num}`) // + `${page_num}`
+    fetch(`/api/attractions?page=${page_num}`) // + `${page_num}`
     .then(function (response){
         return response.json();
     })
@@ -24,8 +26,10 @@ function get_data(){
         let data_list = data.data
         // console.log(data_list[0].images[0])
         // console.log(data_list.length)
+        console.log(data.nextPage)
+
         const listEnd = document.querySelector('.footer');
-        
+       
         
         const append_view = () =>{
 
@@ -33,54 +37,87 @@ function get_data(){
 
             for (i = 0 ; i < data_list.length  ; i ++ ){
 
-                // record the counting for i
+               // create container square
+
+               let square = document.createElement("div");
+               square.className = "square";
+               let content = document.querySelector(".content-1");
+               content.appendChild(square);
 
                 
     
                 // attraction images
-                first_pic = data_list[i].images[0]              
-                let div_box = document.querySelectorAll("#img-control")            
-                div_box[i].src = first_pic
+
+                first_pic = data_list[i].images[0];              
+                let pic_box = document.createElement("img");
+                pic_box.id = "img-control";
+                pic_box.src = first_pic;
+                square.appendChild(pic_box);
          
     
     
     
                 // attraction name (title)
-                let main_title = document.createTextNode(data_list[i].name);
+
+                let title_text = document.createTextNode(data_list[i].name);
                 
-                let title_text = document.querySelectorAll(".title");
-                title_text[i].appendChild(main_title);
+                let title = document.createElement("div");
+                title.className = "title";
+                title.appendChild(title_text);
+                square.appendChild(title)
+
                 
                 // attraction mrt
                 let mrt = document.createTextNode(data_list[i].mrt);
-                let mrt_box = document.querySelectorAll(".mrt");
-                mrt_box[i].appendChild(mrt);
+                let mrt_box = document.createElement("div");
+                mrt_box.className = "mrt" ;               
+                mrt_box.appendChild(mrt);
+                
+
     
                 // attraction category (tag)
                 let category = document.createTextNode(data_list[i].category);
-                let cat_box = document.querySelectorAll(".tag")
-                cat_box[i].appendChild(category)
+                let cat_box = document.createElement("div");
+                cat_box.className = "tag";
+                cat_box.appendChild(category);
+
+                // container detail
+                let detail = document.createElement("div");
+                detail.className = "detail";
+                detail.appendChild(mrt_box);
+                detail.appendChild(cat_box)
+                square.appendChild(detail)
 
 
-                count_i++
-                console.log(count_i)
+                // record the counting for i
+                // count_i++
+                // console.log(count_i);
 
                 
 
-
-
+ 
                 
     
             };
-            
+
             page_num ++
-            
+            console.log(page_num)
+            // // number = page_num ++
+
+
         };
-        append_view();
+
+        // append_view();
+                   
+  
 
         const load_view = () => {
-            for (let i = 0 ; i < data_list.length ; ++i ) {
-            append_view();
+
+
+            for (let k = 0 ; k <= data.nextPage ; ++k ) { //k < data_list.length
+                page_num ++
+                console.log(page_num)
+                append_view();
             };
         };
 
@@ -90,7 +127,7 @@ function get_data(){
             console.log(entry);
                 // Load more articles;
                 if (entry.isIntersecting) {
-                    if (i < 10) {
+                    if ( page_num <= data.nextPage ) {  //代判斷中
                         load_view();        
                     } else {
                     observer.unobserve(listEnd);
@@ -103,8 +140,9 @@ function get_data(){
         const observer = new IntersectionObserver(callback, {
             threshold: 0,
         });
-        observer.observe(listEnd);       
-
+        observer.observe(listEnd);    
+        
+        
     }); 
     
 };
@@ -115,9 +153,6 @@ function get_data(){
 get_data();
 
 
-// function run() {
-//     window.scroll(1000);
-// }
 
 // 看我的scroll高度用的，
 // window.addEventListener('scroll', () => {
